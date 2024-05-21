3095f3cb836fc8f4090b027ebc895879bf9eabe9
spuckydaslama <daniel.3bert@gmail.com>
2024-05-21 22:22:52 +0200

    try loading md file

---
+++ src/lib/data/recipes.md
@@ -0,0 +1,12 @@
+# Recipe 1
+some text 1
+
+---SPLIT---
+
+# Recipe 2
+some text 2
+
+---SPLIT---
+
+# Recipe 3
+some text 3
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	// import recipes.md and split the text by '---'
+	import recipes from '$lib/data/recipes.md?raw';
+
+	const allRecipes = recipes.split('---SPLIT---');
+</script>
+
+<main>
+	{#each allRecipes as recipe}
+		<pre>{recipe}</pre>
+	{/each}
+</main>
