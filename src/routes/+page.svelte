4b30b55ee70db43e47c3bc26fecc446f0a41a12b
spuckydaslama <daniel.3bert@gmail.com>
2024-05-23 05:34:09 +0200

    render shuffled recipes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,35 @@
 <script lang="ts">
+	import * as Carousel from '$lib/components/ui/carousel';
 	import recipes from '$lib/data/recipes.md?raw';
-	import {marked} from "marked";
 
-	const allRecipes = recipes.split('---SPLIT---');
-	const recipesHtml = allRecipes.map(recipe => marked(recipe));
+	import { marked } from 'marked';
+	import {onMount} from "svelte";
+	import {shuffle} from "./shuffle";
+
+	const allRecipesMarkdown = recipes.split('---SPLIT---');
+	const allRecipesAsHtml = allRecipesMarkdown.map((recipeMarkdown) => marked(recipeMarkdown) as string);
+
+	let randomRecipes: string[] = $state([]);
+	onMount(() => {
+		const recipes = [...allRecipesAsHtml];
+		shuffle(recipes);
+		randomRecipes = recipes;
+	})
 </script>
 
 <main>
-	{#each recipesHtml as recipe}
-		{@html recipe}
-	{/each}
+	<script lang="ts">
+	</script>
+
+	<Carousel.Root class="w-full" opts={{loop: true}}>
+		<Carousel.Content>
+			{#each randomRecipes as recipe}
+				<Carousel.Item>
+					<div class="p-1 prose border-amber-600 border-2 border-solid">
+						{@html recipe}
+					</div>
+				</Carousel.Item>
+			{/each}
+		</Carousel.Content>
+	</Carousel.Root>
 </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,9 @@ const config: Config = {
 			}
 		}
 	},
+	plugins: [
+		require("@tailwindcss/typography"),
+	],
 };
 
 export default config;
